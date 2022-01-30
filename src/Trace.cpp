c6d9f72c2a202e35890109c42cd267190be46101
Joost Kraaijeveld <J.Kraaijeveld@Askesis.nl>
2022-01-30 15:19:15 +0100

    Fixed copy&paste error

--- src/Trace.cpp
+++ src/Trace.cpp
@@ -181,6 +181,7 @@ namespace Base
 				if(threadIndentionLevels.find(currentThreadId)!=threadIndentionLevels.end())
 				#else
 				if(!threadIndentionLevels.contains(currentThreadId)) // @suppress("Method cannot be resolved")
+				#endif
 				{
 					threadIndentionLevels.insert(std::make_pair(currentThreadId,0));
 				}
